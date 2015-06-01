914def1ac97d8196250a2273c92e652ed5e63381
darryl <d.amatsetam@gmail.com>
2015-06-01 22:24:47 +0200

    added correct logger

--- src/main/java/EventLoader.java
+++ src/main/java/EventLoader.java
@@ -1,3 +1,4 @@
+import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -12,7 +13,7 @@ abstract public class EventLoader extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        logger = getLogger();
+        logger = Bukkit.getLogger();
         getServer().getPluginManager().registerEvents(this, this);
     }
 
