3803c09f7a5d129a4c26c484b1cfe7b10f5e571e
darryl <d.amatsetam@gmail.com>
2015-06-01 19:24:20 +0200

    added correct lib, maven support

--- src/main/java/BlockSpawnLoader.java
+++ src/main/java/BlockSpawnLoader.java
@@ -1,6 +1,7 @@
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.server.MapInitializeEvent;
 
@@ -14,9 +15,14 @@ public class BlockSpawnLoader extends EventPlugin {
         Coords blockCoords;
         for (String line : CoordsData.getData()) {
             String[] split = line.split(";");
-            blockCoords = new Coords(Integer.parseInt(split[1]), Integer.parseInt(split[2]), 85, Integer.parseInt(split[0]));
-            Location loc = new Location(world, blockCoords.getX(), blockCoords.getY(), blockCoords.getZ(), 0f, 0f);
-            world.getBlockAt(loc).setType(Material.OBSIDIAN);
+            for (int i = 64; i < 256; i++) {
+                blockCoords = new Coords(Integer.parseInt(split[1]), Integer.parseInt(split[2]), i, Integer.parseInt(split[0]));
+                Location loc = new Location(world, blockCoords.getX(), blockCoords.getY(), blockCoords.getZ(), 0f, 0f);
+                Block currentBlock = world.getBlockAt(loc);
+                if (currentBlock.getType().equals(Material.AIR)) {
+                    currentBlock.setType(Material.OBSIDIAN);
+                }
+            }
         }
     }
 }
